a100ca9554c092c862f75d672f6247f063fd02f1
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2022-11-28 14:33:39 +0200

    Added small hack for prettier plugins
    https://github.com/trivago/prettier-plugin-sort-imports/issues/117

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// https://github.com/trivago/prettier-plugin-sort-imports/issues/117
+
 /** @type {import("prettier").Config} */
+
+const pluginSortImports = require("@trivago/prettier-plugin-sort-imports")
+const pluginTailwindcss = require("prettier-plugin-tailwindcss")
+
+/** @type {import("prettier").Parser}  */
+const myParser = {
+  ...pluginSortImports.parsers.typescript,
+  parse: pluginTailwindcss.parsers.typescript.parse,
+}
+
+/** @type {import("prettier").Plugin}  */
+const myPlugin = {
+  parsers: {
+    typescript: myParser,
+  },
+}
+
 module.exports = {
   trailingComma: "es5",
   tabWidth: 2,
@@ -16,8 +37,5 @@ module.exports = {
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  plugins: [
-    require.resolve("@trivago/prettier-plugin-sort-imports"),
-    require.resolve("prettier-plugin-tailwindcss"),
-  ],
+  plugins: [myPlugin],
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 import "../styles/globals.css";
+
+const queryClient = new QueryClient();
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,

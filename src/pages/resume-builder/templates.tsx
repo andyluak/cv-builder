d131704a62db620c1507ab4aba553e7af13d9820
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2022-12-05 13:00:14 +0200

    Created content json file for builder

--- src/pages/resume-builder/templates.tsx
+++ src/pages/resume-builder/templates.tsx
@@ -1,35 +1,27 @@
+import resumeBuilderContent from "content/resumeBuilderContent.json";
+import Link from "next/link";
 import React, { useState } from "react";
 import { useResumeContext } from "src/context/ResumeContext";
 
 import NotionTemplate from "src/components/NotionTemplate";
 import MainLayout from "src/components/layout/Main";
 import Input from "src/components/ui/Input";
-import Link from "next/link";
 
 function Template() {
   const [showButton, setShowButton] = useState(false);
-  const { template, setTemplate, userInfo, setUserInfo } = useResumeContext();
+  const { setTemplate, userInfo, setUserInfo } = useResumeContext();
 
   const handleUserInfoChange = (e: { target: { name: any; value: any } }) => {
     setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
 
-  console.log(userInfo);
-
-  console.log(template);
   return (
     <div className="flex flex-col items-center gap-12">
       <div className="flex flex-col gap-2">
         <h1 className="max-w-3xl text-4xl">
-          {
-            "Choose a template and let's get started on your next job-winning resume"
-          }
+          {resumeBuilderContent.template.header}
         </h1>
-        <p className="text-gray-300">
-          {
-            "Don't worry, you can always change your template later if you want to try something different."
-          }
-        </p>
+        <p className="text-gray-300">{resumeBuilderContent.template.body}</p>
       </div>
       <div className="flex w-full flex-row justify-center gap-4">
         <Input
@@ -51,35 +43,36 @@ function Template() {
           onChange={handleUserInfoChange}
         />
       </div>
-
-      <div
-        className="relative w-[200px] cursor-pointer overflow-hidden outline-red-500 hover:outline-double hover:outline-2"
-        onMouseEnter={() => setShowButton(true)}
-        onMouseLeave={() => setShowButton(false)}
-      >
-        <div className="h-80 w-[400px] select-none overflow-hidden bg-red-400 text-[9px]">
-          <NotionTemplate
-            style={{
-              transform: "scale(0.5,0.5)",
-              transformOrigin: "top left",
-            }}
-            firstName={userInfo.firstName}
-            lastName={userInfo.lastName}
-          />
+      <div className="grid grid-cols-3 gap-16">
+        <div
+          className="relative w-[200px] cursor-pointer overflow-hidden outline-red-500 hover:outline-double hover:outline-2"
+          onMouseEnter={() => setShowButton(true)}
+          onMouseLeave={() => setShowButton(false)}
+        >
+          <div className="h-80 w-[400px] select-none overflow-hidden bg-red-400 text-[9px]">
+            <NotionTemplate
+              style={{
+                transform: "scale(0.5,0.5)",
+                transformOrigin: "top left",
+              }}
+              firstName={userInfo.firstName}
+              lastName={userInfo.lastName}
+            />
+          </div>
+          {showButton && (
+            <>
+              <div className="absolute top-0 left-0 bottom-0 right-0 bg-gray-800 opacity-50"></div>
+              <Link href="/resume-builder/basic-information">
+                <button
+                  className="absolute left-1/2 top-1/2 min-w-[150px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-800 p-2 text-gray-300 hover:outline-double hover:outline-2 hover:outline-gray-200"
+                  onClick={() => setTemplate("notion")}
+                >
+                  Select Template
+                </button>
+              </Link>
+            </>
+          )}
         </div>
-        {showButton && (
-          <>
-            <div className="absolute top-0 left-0 bottom-0 right-0 bg-gray-800 opacity-50"></div>
-            <Link href="/resume-builder/basic-information">
-            <button
-              className="absolute left-1/2 top-1/2 min-w-[150px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-800 p-2 text-gray-300 hover:outline-double hover:outline-2 hover:outline-gray-200"
-              onClick={() => setTemplate("notion")}
-            >
-              Select Template
-            </button>
-            </Link>
-          </>
-        )}
       </div>
     </div>
   );

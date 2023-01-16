56731c68d411090cfc0896d1902c3071558d6ead
Alex Tirim <alexandru.tirim@yahoo.com>
2023-01-16 18:32:19 +0200

    Resume page

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -7,9 +7,8 @@ type Props = {
 function Loading({ message }: Props) {
   return (
     <div>
-      <div className="flex flex-col items-center gap-2 min-h-screen justify-center">
+      <div className="flex min-h-screen flex-col items-center justify-center gap-2">
         <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-gray-900"></div>
-        <p>Loading...</p>
 
         {message && <p>{message}</p>}
       </div>
--- src/pages/dashboard/resume/[id].tsx
+++ src/pages/dashboard/resume/[id].tsx
@@ -1,17 +1,40 @@
+import * as Accordion from "@radix-ui/react-accordion";
 import { useRouter } from "next/router";
 import React from "react";
+import useResume from "src/queries/useResume";
+
 import MainLayout from "src/components/layout/Main";
 import Loading from "src/components/ui/Loading";
-import useResume from "src/queries/useResume";
 
 function Resume() {
   const router = useRouter();
   const id = router.query.id as string;
   const { isLoading, isError, resume } = useResume(id);
 
-  if( isLoading) return <Loading message="Loading resume..." />;
+  if (isLoading) return <Loading message="Loading resume..." />;
 
-  return <div>Resume</div>;
+  if (isError) return <div>Something went wrong</div>;
+
+  return (
+    <div>
+      <Accordion.Root type="single">
+        <Accordion.Item value="1">
+          <Accordion.Header>
+            <Accordion.Trigger>Is it unstyled?</Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            <div>Job SAl</div>
+          </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="2">
+          <Accordion.Header>Education</Accordion.Header>
+          <Accordion.Content>
+            <div>Education</div>
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </div>
+  );
 }
 
 export default Resume;

9a8f2ece4902ee5361957a2ba5e33a63ba9f3119
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2023-01-22 13:11:17 +0200

    clean up component

--- src/pages/resume-builder/basic-information.tsx
+++ src/pages/resume-builder/basic-information.tsx
@@ -39,44 +39,24 @@ function BasicInformationPage() {
         controlled
       />
 
-      <div
-        className="relative w-[200px] cursor-pointer overflow-hidden outline-red-500 hover:outline-double hover:outline-2"
-        onMouseEnter={() => setShowButton(true)}
-        onMouseLeave={() => setShowButton(false)}
-      >
-        <div className="h-80 w-[400px] select-none overflow-hidden bg-red-400 text-[9px]">
-          <TemplateDisplayer
-            key={template}
-            LinkedButton={() => <></>}
-            template={template}
-            style={{
-              width: "100%",
-              maxWidth: "800px",
-              height: "100%",
-              fontSize: "0.3rem",
-            }}
-            firstName={userInfo.firstName}
-            lastName={userInfo.lastName}
-            position={userInfo.position}
-            phone={userInfo.phone}
-            email={userInfo.email}
-            profileDescription={userInfo.profileDescription}
-            isPreview
-          />
-        </div>
-        {showButton && (
-          <>
-            <div className="absolute top-0 left-0 bottom-0 right-0 bg-gray-800 opacity-50"></div>
-            <Button
-              className="absolute left-1/2 top-1/2 min-w-[150px] -translate-x-1/2 -translate-y-1/2"
-              variant="primary"
-              size="lg"
-            >
-              Preview
-            </Button>
-          </>
-        )}
-      </div>
+      <TemplateDisplayer
+        key={template}
+        LinkedButton={() => <></>}
+        template={template}
+        style={{
+          width: "100%",
+          maxWidth: "800px",
+          height: "100%",
+          fontSize: "0.3rem",
+        }}
+        firstName={userInfo.firstName}
+        lastName={userInfo.lastName}
+        position={userInfo.position}
+        phone={userInfo.phone}
+        email={userInfo.email}
+        profileDescription={userInfo.profileDescription}
+        isPreview
+      />
       <div className="flex flex-row items-center justify-center gap-12">
         <Link href="/resume-builder/templates">
           <Button variant="secondary" size="lg">
